4236d81508430c5100a1eab212b34bd68b0c622e
Justin Fay <justinfay501@gmail.com>
2024-11-17 15:34:38 -0500

    Running swagger

--- OCR/Program.cs
+++ OCR/Program.cs
@@ -29,9 +29,15 @@ public class Program
         builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
         builder.Services.AddExceptionHandler<ExceptionHandler>();
         
+        builder.Services.AddControllers();
+        
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
         builder.Services.UseOcrServices();
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
@@ -39,6 +45,8 @@ public class Program
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseExceptionHandler(options => { });
