2d14efad7a48bf04ba15bb8301a6e1108906ab0b
Justin Fay <justinfay501@gmail.com>
2024-11-17 15:15:45 -0500

    init:

---
+++ OCR/Services/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace OCR.Services.Extensions;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection UseOcrServices(this IServiceCollection services)
+    {
+        services.AddScoped<IVisionService, VisionService>(provider =>
+        {
+            var key = Environment.GetEnvironmentVariable("VISION_KEY");
+            var endpoint = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
+            var logger = provider.GetRequiredService<ILogger<VisionService>>();
+            
+            return new VisionService(
+                endpoint ?? throw new ArgumentException("No VISION_KEY Env var"), 
+                key ?? throw new ArgumentException("No VISION_ENDPOINT Env var"), 
+                logger);
+        });
+
+        return services;
+    }
+}
---
+++ OCR/Services/VisionService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace OCR.Services;
+
+public interface IVisionService
+{
+    /// <summary>
+    /// Reads a file from bytes and returns the text
+    /// </summary>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    Task<List<string>> ReadFileFromBytesAsync(byte[] file);
+    Task ReadFileFromUrlAsync(string url);
+}
+
+
+public class VisionService: IVisionService
+{
+    private string _visionKey;
+    private string _visionEndpoint;
+    private readonly ILogger<VisionService> _logger;
+    private ComputerVisionClient _client;
+    
+    public VisionService(string visionEndpoint, string visionKey, ILogger<VisionService> logger)
+    {
+        _visionEndpoint = visionEndpoint;
+        _visionKey = visionKey;
+        _logger = logger;
+        _client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(visionKey))
+        {
+            Endpoint = visionEndpoint
+        };
+    }
+
+    public async Task<List<string>> ReadFileFromBytesAsync(byte[] file)
+    {
+        _logger.LogInformation("Reading file from bytes");
+
+        using var stream = new MemoryStream(file);
+        var result = await _client.RecognizePrintedTextInStreamAsync(true, stream,
+            OcrLanguages.En);
+
+        return result.Regions.SelectMany(region => region.Lines)
+            .SelectMany(line => line.Words)
+            .Select(word => word.Text)
+            .ToList();
+    }
+
+    public Task ReadFileFromUrlAsync(string url)
+    {
+        throw new NotImplementedException();
+    }
+}
