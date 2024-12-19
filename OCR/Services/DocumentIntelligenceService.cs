3e7bfc030ca6f1915876edf12d8a34988ad7520c
Justin Fay <justin.fay@hope.edu>
2024-12-18 21:17:40 -0500

    String extensions, space splitting, and special character filtering

--- OCR/Services/DocumentIntelligenceService.cs
+++ OCR/Services/DocumentIntelligenceService.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.AI.FormRecognizer.DocumentAnalysis;
+using OCR.Services.Extensions;
 
 namespace OCR.Services;
 
@@ -31,10 +32,21 @@ public class DocumentIntelligenceService : IDocumentIntelligenceService
         _logger.LogInformation("Analyzing document");
 
         var result = await _client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-document", file.OpenReadStream());
-        
-        return result.Value.Pages.SelectMany(page => page.Lines)
-            .Select(line => line.Content)
+
+        var words = GetLinesFromPages(result.Value.Pages)
+            .SplitBySpace()
+            .FilterSpecialCharacters()
             .ToList();
+        
+        return words;
     }
+    
+    private static IEnumerable<string> GetLinesFromPages(IReadOnlyList<DocumentPage> pages)
+    {
+        var lines = pages.SelectMany(page => page.Lines)
+            .Select(line => line.Content);
+        return lines;
+    }
+ 
     
 }
---
+++ OCR/Services/Extensions/StringEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace OCR.Services.Extensions;
+
+public static class StringEnumerableExtensions
+{
+    public static IEnumerable<string> SplitBySpace(this IEnumerable<string> lines)
+    {
+        return lines.SelectMany(line => line.Split(' '));
+    }
+
+    public static IEnumerable<string> FilterSpecialCharacters(this IEnumerable<string> words)
+    {
+        return words
+            .Select(word => new string(word.Where(char.IsLetterOrDigit).ToArray()))
+            .Where(filteredWord => !string.IsNullOrEmpty(filteredWord));
+    }
+}
